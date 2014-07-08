c471e55fb533fedf675d50249554f1833048672d
Michael Koltashew <snake_kmm@mail.ru>
2014-07-09 00:29:55 +0300

    created html content and js initial content, need converting functions in main js-object

--- JavascriptFirstTask/Content/Scripts/DateDisplayFormatter.js
+++ JavascriptFirstTask/Content/Scripts/DateDisplayFormatter.js
@@ -1,1 +1,41 @@
-﻿
+﻿function dateFormatter() {
+
+    var outputButton = document.getElementById("format");
+
+    outputButton.onclick = function checkInput() {
+        var inputContext = document.getElementById("input");
+        var outputContext = document.getElementById("output");
+        var dateContext = document.getElementById("date");
+        var inputFormatTypeList = document.getElementById("inputFormatTypeList");
+        var inputDate = document.getElementById("dateInput").value;
+        if (inputDate != "") {
+            dateContext.innerHTML = "input data: " + inputDate;
+            document.getElementById("dateInputLabel").innerHTML = '';
+        } else {
+            dateContext.innerHTML = '';
+            document.getElementById("dateInputLabel").innerHTML = 'no date';
+        }
+        var selectedInputFormatValue = inputFormatTypeList.options[inputFormatTypeList.selectedIndex].value;
+
+        if (selectedInputFormatValue === "Not selected") {
+            document.getElementById("inputFormatTypeListLabel").innerHTML = 'not selected';
+            inputContext.innerHTML = '';
+        } else {
+            inputContext.innerHTML = "Input format: " + selectedInputFormatValue;
+            document.getElementById("inputFormatTypeListLabel").innerHTML = '';
+        }
+        var outputFormatTypeList = document.getElementById("outputFormatTypeList");
+        var selectedOutputFormatValue = outputFormatTypeList.options[outputFormatTypeList.selectedIndex].value;
+
+        if (selectedOutputFormatValue === "Not selected") {
+            document.getElementById("outputFormatTypeListLabel").innerHTML = 'not selected';
+            outputContext.innerHTML = '';
+        } else {
+            outputContext.innerHTML = "Output format: " + selectedOutputFormatValue;
+            document.getElementById("outputFormatTypeListLabel").innerHTML = '';
+        }
+
+    };
+};
+
+dateFormatter();
