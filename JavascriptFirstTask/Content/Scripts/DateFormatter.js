d0fdd1d1c34ce6d7dbf501ef223d55817fe23c9f
Michael Koltashew <snake_kmm@mail.ru>
2014-07-09 20:19:04 +0300

    Added parser of initial string to parts according to input format

--- JavascriptFirstTask/Content/Scripts/DateFormatter.js
+++ JavascriptFirstTask/Content/Scripts/DateFormatter.js
@@ -1,7 +1,75 @@
 ﻿var dateFormatter = {
-    
     formatDate: function (inputDate, selectedInputFormatValue, selectedOutputFormatValue) {
-        return (inputDate + " " + selectedInputFormatValue + " " + selectedOutputFormatValue);
+        var day;
+        var month;
+        var year;
+        var months = [
+                    { January: "01" },
+                    { February: "02" },
+                    { March: "03" },
+                    { April: "04" },
+                    { May: "05" },
+                    { June: "06" },
+                    { July: "07" },
+                    { August: "08" },
+                    { September: "09" },
+                    { October: "10" },
+                    { November: "11" },
+                    { December: "12" }
+        ];
+
+        function getComponentsFromHoursFormat() {
+            //initial time from 1/1/1970 03-00(GMT+3) 
+            var msInHours = parseInt(inputDate - 3, 10) * 60 * 60 * 1000;
+            var dateFromHours = new Date(msInHours);
+            day = dateFromHours.getDate() - 1;
+            month = dateFromHours.getMonth();
+            year = dateFromHours.getFullYear() - 1970;
+        }
+
+        function getComponentsFromYyyymmddFormat() {
+            day = inputDate.substring(6, 8);
+            month = inputDate.substring(4, 6);
+            year = inputDate.substring(0, 4);
+        }
+
+        function getComponentsFromYyyyddmmFormat() {
+            day = inputDate.substring(4, 6);
+            month = inputDate.substring(6, 8);
+            year = inputDate.substring(0, 4);
+        }
+
+        function getComponentsFromDdmmyyyyFormat() {
+            day = inputDate.substring(0, 2);
+            month = inputDate.substring(2, 4);
+            year = inputDate.substring(4, 8);
+        }
+
+        function getComponentsFromMmddyyyyFormat() {
+            day = inputDate.substring(2, 4);
+            month = inputDate.substring(0, 2);
+            year = inputDate.substring(4, 8);
+        }
+
+        switch (selectedInputFormatValue) {
+
+            case "hours":
+                getComponentsFromHoursFormat(); break;
+            case "YYYYMMDD":
+                getComponentsFromYyyymmddFormat(); break;
+            case "YYYYDDMM":
+                getComponentsFromYyyyddmmFormat(); break;
+            case "DDMMYYYY":
+                getComponentsFromDdmmyyyyFormat(); break;
+            case "MMDDYYYY":
+                getComponentsFromMmddyyyyFormat(); break;
+        }
+
+        if (day <= 31 && month <= 12) {
+            return ("day: " + day + "month: " + month + "year: " + year);
+        } else {
+            return false;
+        }
     }
 
-}
+};
--- JavascriptFirstTask/Content/Scripts/FormatDate.js
+++ JavascriptFirstTask/Content/Scripts/FormatDate.js
@@ -1,44 +1,61 @@
 ﻿Window.onload = (function () {
 
     var outputButton = document.getElementById("format");
+
+    var inputFormatTypeList = document.getElementById("inputFormatTypeList");
+    var outputFormatTypeList = document.getElementById("outputFormatTypeList");
+
+    var dateInputLabel = document.getElementById("dateInputLabel");
+    var inputFormatTypeListLabel = document.getElementById("inputFormatTypeListLabel");
+    var outputFormatTypeListLabel = document.getElementById("outputFormatTypeListLabel");
+
     var inputLog = document.getElementById("input");
     var outputLog = document.getElementById("output");
     var dateLog = document.getElementById("date");
     var resultLog = document.getElementById("result");
 
-    var inputFormatTypeList = document.getElementById("inputFormatTypeList");
-    var outputFormatTypeList = document.getElementById("outputFormatTypeList");
-
+    function isDateStringValid(inputDate, selectedInputFormatValue) {
+        var regExpForDateString = /\d{8}/;
+        var regExpForHoursString = /^[1-9][0-9]*$/;
+        if (selectedInputFormatValue === "hours") {
+            return regExpForHoursString.exec(inputDate);
+        } else {
+            return regExpForDateString.exec(inputDate);
+        }
+    };
     outputButton.onclick = function validateInput() {
 
         var selectedInputFormatValue = inputFormatTypeList.options[inputFormatTypeList.selectedIndex].value;
         var selectedOutputFormatValue = outputFormatTypeList.options[outputFormatTypeList.selectedIndex].value;
         var inputDate = document.getElementById("dateInput").value;
+        var isDateValid = isDateStringValid(inputDate, selectedInputFormatValue) && dateFormatter.formatDate(inputDate, selectedInputFormatValue, selectedOutputFormatValue);
 
-        if (inputDate !== "") {
-            dateLog.innerHTML = "input data: " + inputDate;
-            document.getElementById("dateInputLabel").innerHTML = '';
-        } else {
+        if (inputDate === "") {
             dateLog.innerHTML = '';
-            document.getElementById("dateInputLabel").innerHTML = 'no date';
+            dateInputLabel.innerHTML = 'no date';
+        } else if (!isDateValid) {
+            dateInputLabel.innerHTML = 'invalid string';
+        } else {
+            dateLog.innerHTML = "input data: " + inputDate;
+            dateInputLabel.innerHTML = '';
         }
 
         if (selectedInputFormatValue === "Not selected") {
-            document.getElementById("inputFormatTypeListLabel").innerHTML = 'not input format';
+            inputFormatTypeListLabel.innerHTML = 'not input format';
             inputLog.innerHTML = '';
         } else {
             inputLog.innerHTML = "Input format: " + selectedInputFormatValue;
-            document.getElementById("inputFormatTypeListLabel").innerHTML = '';
+            inputFormatTypeListLabel.innerHTML = '';
         }
 
         if (selectedOutputFormatValue === "Not selected") {
-            document.getElementById("outputFormatTypeListLabel").innerHTML = 'no output format';
+            outputFormatTypeListLabel.innerHTML = 'no output format';
             outputLog.innerHTML = '';
         } else {
             outputLog.innerHTML = "Output format: " + selectedOutputFormatValue;
-            document.getElementById("outputFormatTypeListLabel").innerHTML = '';
+            outputFormatTypeListLabel.innerHTML = '';
         }
-        if (inputDate !== "" && selectedInputFormatValue !== "Not selected" && selectedOutputFormatValue !== "Not selected") {
+        if (inputDate !== "" && isDateValid && selectedInputFormatValue !== "Not selected" && selectedOutputFormatValue !== "Not selected") {
             resultLog.innerHTML = 'Result: '+ dateFormatter.formatDate(inputDate, selectedInputFormatValue, selectedOutputFormatValue);
         } else {
             resultLog.innerHTML = '';
